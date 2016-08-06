97235a348bf49e65bafca48d4cea3de8256aabe3
Wes Basinger <wbasinger@villagetechschools.org>
2016-08-05 20:09:28 -0500

    Going 3 callbacks deep to get all the data, now I 've got to piece it together enough to get individual submissions!

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var request = require('superagent');
 var readlineSync = require('readline-sync');
 
+var students, assignments;
+
 var url = "https://script.google.com/macros/s/AKfycbzGmvkUXGTvNK0ChTl9EMk9BenhwbtLHrjRjbaPauIW5CNblb0/exec";
 console.log("Loading class lists...");
 // kick off the fun by getting a class list
@@ -15,12 +17,22 @@ request.get(url + "?method=getClasses").end(function(err, res) {
     });
     index = readlineSync.keyInSelect(names, "Which class?");
     var selected = classList[index];
+    var callbackCount = 0;
     request.get(url + "?method=getStudents&courseId=" + selected.id).end(function(err, res) {
       if (err) {
         console.log(err);
       } else {
-        console.log(res.body);
+        var students = res.body;
+        request.get(url + "?method=getAssignments&courseId=" + selected.id).end(function(err, res) {
+          if (err) {
+            console.log(err);
+          } else {
+            var assignments = res.body;
+            console.log(assignments);
+          }
+        });
       }
     });
+
   }
 });
